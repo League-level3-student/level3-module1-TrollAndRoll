d3242ca9535239ef279ceca1fab61c0d2a0718c3
BuildTools <unconfigured@null.spigotmc.org>
2021-11-26 01:11:03 -0800

    Memory Match IP

--- src/_04_Memory_Match/GameBoard.java
+++ src/_04_Memory_Match/GameBoard.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,7 +19,7 @@ public class GameBoard extends JFrame implements ActionListener {
     static Card secondSelectedCard = null;
     
     // 1. Initialize TOTAL_CARDS to 2;
-    static int TOTAL_CARDS = 0;
+    static int TOTAL_CARDS = 52;
     
     ArrayList<Card> cards;
     
@@ -42,34 +43,50 @@ public class GameBoard extends JFrame implements ActionListener {
         }
         
         // 2. Initialize the ArrayList of Cards declared above
-        
+        cards = new ArrayList<Card>();
         
         // 3. Create TOTAL_CARDS number of objects each with a value of 1.
         //    Also, add action listeners to each Card object and then add each
         //    of the Card objects to the ArrayList of Cards.
-        
+        int numOfSuits = 4;
+        int cardNum = 1;
+        for(int i = 1; i <= numOfSuits; i++) {
+        	cardNum = i;
+        	for(int j = 1; j <= TOTAL_CARDS/numOfSuits; j++) {
+        		Card newCard = new Card(j);
+            	newCard.setFaceUpIcon(Card.cardImagesPath + (cardNum) + ".png");
+            	System.out.println(i + " " + j + " " + cardNum);
+            	cardNum += 4;
+            	newCard.addActionListener(this);
+            	cards.add(newCard);
+        	}
+        }
         
         // 4. Use Collections.shuffle() method to randomize the order of
         //    the cards in the ArrayList
-        
+        Collections.shuffle(cards);
         
         // 5. Initialize the panel variable declared above
-        
+        panel = new JPanel();
         
         // 6. Add all of the Card objects to the panel
-        
+        for(Card c : cards) {
+        	panel.add(c);
+        }
         
         // 7. Call the setupGui() method to set up the frame
-        
+        setupGui(cards);
         
         // 8. Call the startGame() method to start the game
-        
+        startGame();
     }
 
     // 9. Fill in the drawCards method to draw all the cards in the ArrayList.
     //    Run your code and verify 2 cards are displayed and the game works.
     public void drawCards() {
-        
+        for(Card c : cards) {
+        	c.draw();
+        }
     }
     
     // 10. 
